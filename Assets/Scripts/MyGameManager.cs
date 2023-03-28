675d2cb32667feb2ee982b816bfe8f2743e26976
ralp99 <ralp99@hotmail.com>
2023-03-28 04:55:33 -0700

    [RA] both tanks load and operate properly (so far as they are able) in multiplayer scene

--- Assets/Scripts/MyGameManager.cs
+++ Assets/Scripts/MyGameManager.cs
@@ -9,7 +9,8 @@ public class MyGameManager : MonoBehaviour
 
     public GameObject TankModel;
     GameObject Player1tank;
-    public TankController TankA, TankB, TankC;
+    public TankController TankA, TankB, TankC, TankD;
+    public List<TankController> ActiveTanks = new List<TankController>();
     public Transform[] TankSpawnPoints;
     public float TankDriveSpeed = 1.0f;
     public float TankSpinSpeed = 1.0f;
@@ -25,11 +26,11 @@ public class MyGameManager : MonoBehaviour
 
     void Start()
     {
-        view = GetComponent<PhotonView>();
-        if (view.IsMine)
-        {
+        //view = GetComponent<PhotonView>();
+       // if (view.IsMine)
+        //{
             GameBegin();
-        }
+       // }
     }
 
 
@@ -44,9 +45,36 @@ public class MyGameManager : MonoBehaviour
 
         Vector3 loadRotation = new Vector3(-90, 0, 0);
         GameObject TankAObject = PhotonNetwork.Instantiate(TankModel.name, TankASpawnPoint.transform.position, Quaternion.Euler(loadRotation));
+        TankAObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+        TankController thisTank = TankAObject.GetComponent<TankController>();
+
+        ActiveTanks.Add(thisTank);
+
+        
+        /*
+        if (ActiveTanks.IndexOf(thisTank) == 0)
+        {
+            TankA = thisTank;
+        }
+
+        if (ActiveTanks.IndexOf(thisTank) == 1)
+        {
+            TankB = thisTank;
+        }
+
+        if (ActiveTanks.IndexOf(thisTank) == 2)
+        {
+            TankC = thisTank;
+        }
+
+        if (ActiveTanks.IndexOf(thisTank) == 3)
+        {
+            TankD = thisTank;
+        }
+        */
 
         TankA = TankAObject.GetComponent<TankController>();
-        TankAObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
     }
 
 
